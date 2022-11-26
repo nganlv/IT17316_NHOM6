4e9c3fb92da93addd4a5dc38adc6a7638f0061e3
anndph23528 <117999602+anndph23528@users.noreply.github.com>
2022-11-26 19:25:12 +0700

    update_fix

--- DuAn1/src/Services/QLNhanVien.java
+++ DuAn1/src/Services/QLNhanVien.java
@@ -20,7 +20,11 @@ public interface QLNhanVien {
       String delete(String maNV);
        Boolean checkMa(String maNV);
        List<QuanLyNhanVien> getTims(String ma);
+
 //       List<QuanLyNhanVien> getTimTens(String ten);
        List<QuanLyNhanVien> getLOCs(String cv);
+
+
+       List<QuanLyNhanVien> getTimTens(String ten);
 }
 
